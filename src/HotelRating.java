9192b0e3d27b28abfcdf571971939e9256de90d7
Ernst Fanfan <ernst.r.fanfan@gmail.com>
2019-11-03 10:09:32 -0500

    Added table of int Added header to class Added constructor Initialized table with random numbers between 1-5

--- src/HotelRating.java
+++ src/HotelRating.java
@@ -1,2 +1,21 @@
+//Class: CS 5000
+//Term: Fall 2019
+//Name: Ernst Fanfan
+//Instructor: Dr. Haddad
+//Assignment: 9
+//IDE Name: IntelliJ
 public class HotelRating {
+    private int [] [] table;//rating table
+
+    public HotelRating (int numOfHotels, int numOfYears){//constructor
+        table = new int [numOfHotels][numOfYears];//Assign table rows and columns
+
+        //Initializing table with random numbers with numbers between 1-5
+        for (int rows = 0; rows < table.length; rows++){//loop through rows
+            for (int columns = 0; columns < table[rows].length; columns++){//loop through columns
+                table[rows][columns] = (int)(1 + Math.random()*5);//assign random number between 1-5
+            }
+        }
+    }
+
 }
